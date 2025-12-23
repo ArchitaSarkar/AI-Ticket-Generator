f324cf64f1deb2b6c37ad13149ab4089aee03896
Archita <architasarkar1602@gmail.com>
2025-12-23 17:40:34 +0530

    feat: integrate Tailwind CSS and DaisyUI, remove old styles and components
    - Added Tailwind CSS and DaisyUI to the project dependencies.
    - Removed App.css and App.jsx files to eliminate old styling and components.
    - Updated index.css to use Tailwind CSS.
    - Implemented routing with react-router-dom, including protected routes using a new CheckAuth component.
    - Created Navbar component for navigation with login, signup, and admin links.
    - Added placeholder pages for Admin, Login, Signup, Ticket, and Tickets.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,65 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route, Router } from 'react-router-dom'
+import check_auth from './components/check_auth.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import Tickets from './pages/tickets.jsx'
+import Login from './pages/login.jsx'
+import Admin from './pages/admin.jsx'
+import Ticket from './pages/ticket.jsx'
+import Login from './pages/login.jsx'
+import Signup from './pages/signup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+       <Router>
+         <Route>
+          path='/',
+          element ={
+            <check_auth protected={true}>
+                <Tickets />
+              </check_auth>
+          }
+         </Route>
+
+          <Route>
+          path='/ticket/:id',
+          element ={
+            <check_auth protected={true}>
+                <Ticket/>
+              </check_auth>
+          }
+         </Route>
+
+          <Route>
+          path='/login',
+          element ={
+            <check_auth protected={false}>
+                <Login />
+              </check_auth>
+          }
+         </Route>
+
+          <Route>
+          path='/signup',
+          element ={
+            <check_auth protected={false}>
+                <Signup />
+              </check_auth>
+          }
+         </Route>
+
+          <Route>
+          path='/admin',
+          element ={
+            <check_auth protected={true}>
+                <Admin />
+              </check_auth>
+          }
+         </Route>
+        </Router>
+   </BrowserRouter>
   </StrictMode>,
 )
